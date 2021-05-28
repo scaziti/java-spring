4d36df7e57d2724e213815b8ad5c785ba4cdfb79
Raphael Scaziti <rscaziti@gmail.com>
2021-05-28 12:26:39 -0300

    feat(gerenciador): adicionado primeira página em HTML, criado primeiro servlet

---
+++ gerenciador/src/br/com/alura/gerenciador/servlet/HelloWorldServlet.java
@@ -0,0 +1,27 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>");
+		out.println("Hello, world! Meu primeiro Servlet");
+		out.println("</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("O servlet HelloWorld rodou");
+	}
+}
