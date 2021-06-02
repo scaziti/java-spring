9eb1c8c5e3a65702840b46f6ae43d8eb00b42ae3
Raphael Scaziti <rscaziti@gmail.com>
2021-06-02 15:34:19 -0300

    feat(crud): adicionado remove e altera através de dois servelets

--- gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,17 +1,21 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(Banco.chaveSequencial++);
 		empresa.setNome("Pepsi");
 		
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(Banco.chaveSequencial++);
 		empresa2.setNome("Coca-Cola");
 		
 		lista.add(empresa);
@@ -19,11 +23,33 @@ public class Banco {
 	}
 	
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		lista.add(empresa);
 	}
 	
 	public List<Empresa> getEmpresas(){
 		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if (emp.getId() == id) {
+				it.remove();
+			}
+		}
+	}
+
+	public Empresa buscaEmpresaPeloId(Integer id) {
+		for (Empresa empresa : lista) {
+			if (empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
 	}
 
 }
